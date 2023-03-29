c0187c45a368ddc5dd4a53b69a005f7d5ee09d6e
yuchiXiong <yuchi.xiong@foxmail.com>
2023-03-29 15:15:39 +0800

    feat: 基本的五十音图展示与发音

---
+++ src/data/gojuon.ts
@@ -0,0 +1,663 @@
+//清音
+const seionn = [
+  {
+    hiragana: 'あ',
+    katakana: 'ア',
+    roomaji: 'a',
+    sound: '/sounds/a.mp3'
+  },
+  {
+    hiragana: 'い',
+    katakana: 'イ',
+    roomaji: 'i',
+    sound: '/sounds/i.mp3'
+  },
+  {
+    hiragana: 'う',
+    katakana: 'ウ',
+    roomaji: 'u',
+    sound: '/sounds/u.mp3'
+  },
+  {
+    hiragana: 'え',
+    katakana: 'エ',
+    roomaji: 'e',
+    sound: '/sounds/e.mp3'
+  },
+  {
+    hiragana: 'お',
+    katakana: 'オ',
+    roomaji: 'o',
+    sound: '/sounds/o.mp3'
+  },
+  {
+    hiragana: 'か',
+    katakana: 'カ',
+    roomaji: 'ka',
+    sound: '/sounds/ka.mp3'
+  },
+  {
+    hiragana: 'き',
+    katakana: 'キ',
+    roomaji: 'ki',
+    sound: '/sounds/ki.mp3'
+  },
+  {
+    hiragana: 'く',
+    katakana: 'ク',
+    roomaji: 'ku',
+    sound: '/sounds/ku.mp3'
+  },
+  {
+    hiragana: 'け',
+    katakana: 'ケ',
+    roomaji: 'ke',
+    sound: '/sounds/ke.mp3'
+  },
+  {
+    hiragana: 'こ',
+    katakana: 'コ',
+    roomaji: 'ko',
+    sound: '/sounds/ko.mp3'
+  },
+  {
+    hiragana: 'さ',
+    katakana: 'サ',
+    roomaji: 'sa',
+    sound: '/sounds/sa.mp3'
+  },
+  {
+    hiragana: 'し',
+    katakana: 'シ',
+    roomaji: 'shi',
+    sound: '/sounds/shi.mp3'
+  },
+  {
+    hiragana: 'す',
+    katakana: 'ス',
+    roomaji: 'su',
+    sound: '/sounds/su.mp3'
+  },
+  {
+    hiragana: 'せ',
+    katakana: 'セ',
+    roomaji: 'se',
+    sound: '/sounds/se.mp3'
+  },
+  {
+    hiragana: 'そ',
+    katakana: 'ソ',
+    roomaji: 'so',
+    sound: '/sounds/so.mp3'
+  },
+  {
+    hiragana: 'た',
+    katakana: 'タ',
+    roomaji: 'ta',
+    sound: '/sounds/ta.mp3'
+  },
+  {
+    hiragana: 'ち',
+    katakana: 'チ',
+    roomaji: 'chi',
+    sound: '/sounds/chi.mp3'
+  },
+  {
+    hiragana: 'つ',
+    katakana: 'ツ',
+    roomaji: 'tsu',
+    sound: '/sounds/tsu.mp3'
+  },
+  {
+    hiragana: 'て',
+    katakana: 'テ',
+    roomaji: 'te',
+    sound: '/sounds/te.mp3'
+  },
+  {
+    hiragana: 'と',
+    katakana: 'ト',
+    roomaji: 'to',
+    sound: '/sounds/to.mp3'
+  },
+  {
+    hiragana: 'な',
+    katakana: 'ナ',
+    roomaji: 'na',
+    sound: '/sounds/na.mp3'
+  },
+  {
+    hiragana: 'に',
+    katakana: 'ニ',
+    roomaji: 'ni',
+    sound: '/sounds/ni.mp3'
+  },
+  {
+    hiragana: 'ぬ',
+    katakana: 'ヌ',
+    roomaji: 'nu',
+    sound: '/sounds/nu.mp3'
+  },
+  {
+    hiragana: 'ね',
+    katakana: 'ネ',
+    roomaji: 'ne',
+    sound: '/sounds/ne.mp3'
+  },
+  {
+    hiragana: 'の',
+    katakana: 'ノ',
+    roomaji: 'no',
+    sound: '/sounds/no.mp3'
+  },
+  {
+    hiragana: 'は',
+    katakana: 'ハ',
+    roomaji: 'ha',
+    sound: '/sounds/ha.mp3'
+  },
+  {
+    hiragana: 'ひ',
+    katakana: 'ヒ',
+    roomaji: 'hi',
+    sound: '/sounds/hi.mp3'
+  },
+  {
+    hiragana: 'ふ',
+    katakana: 'フ',
+    roomaji: 'fu',
+    sound: '/sounds/fu.mp3'
+  },
+  {
+    hiragana: 'へ',
+    katakana: 'ヘ',
+    roomaji: 'he',
+    sound: '/sounds/he.mp3'
+  },
+  {
+    hiragana: 'ほ',
+    katakana: 'ホ',
+    roomaji: 'ho',
+    sound: '/sounds/ho.mp3'
+  },
+  {
+    hiragana: 'ま',
+    katakana: 'マ',
+    roomaji: 'ma',
+    sound: '/sounds/ma.mp3'
+  },
+  {
+    hiragana: 'み',
+    katakana: 'ミ',
+    roomaji: 'mi',
+    sound: '/sounds/mi.mp3'
+  },
+  {
+    hiragana: 'む',
+    katakana: 'ム',
+    roomaji: 'mu',
+    sound: '/sounds/mu.mp3'
+  },
+  {
+    hiragana: 'め',
+    katakana: 'メ',
+    roomaji: 'me',
+    sound: '/sounds/me.mp3'
+  },
+  {
+    hiragana: 'も',
+    katakana: 'モ',
+    roomaji: 'mo',
+    sound: '/sounds/mo.mp3'
+  },
+  {
+    hiragana: 'や',
+    katakana: 'ヤ',
+    roomaji: 'ya',
+    sound: '/sounds/ya.mp3'
+  },
+  {
+
+  },
+  {
+    hiragana: 'ゆ',
+    katakana: 'ユ',
+    roomaji: 'yu',
+    sound: '/sounds/yu.mp3'
+  },
+  {
+
+  },
+  {
+    hiragana: 'よ',
+    katakana: 'ヨ',
+    roomaji: 'yo',
+    sound: '/sounds/yo.mp3'
+  },
+  {
+    hiragana: 'ら',
+    katakana: 'ラ',
+    roomaji: 'ra',
+    sound: '/sounds/ra.mp3'
+  },
+  {
+    hiragana: 'り',
+    katakana: 'リ',
+    roomaji: 'ri',
+    sound: '/sounds/ri.mp3'
+  },
+  {
+    hiragana: 'る',
+    katakana: 'ル',
+    roomaji: 'ru',
+    sound: '/sounds/ru.mp3'
+  },
+  {
+    hiragana: 'れ',
+    katakana: 'レ',
+    roomaji: 're',
+    sound: '/sounds/re.mp3'
+  },
+  {
+    hiragana: 'ろ',
+    katakana: 'ロ',
+    roomaji: 'ro',
+    sound: '/sounds/ro.mp3'
+  },
+  {
+    hiragana: 'わ',
+    katakana: 'ワ',
+    roomaji: 'wa',
+    sound: '/sounds/wa.mp3'
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+    hiragana: 'を',
+    katakana: 'ヲ',
+    roomaji: 'wo',
+    sound: '/sounds/wo.mp3'
+  },
+  {
+    hiragana: 'ん',
+    katakana: 'ン',
+    roomaji: 'n',
+    sound: '/sounds/n.mp3'
+  }
+];
+
+//浊音
+const dakuonn = [
+  {
+    hiragana: 'が',
+    katakana: 'ガ',
+    roomaji: 'ga',
+    sound: '/sounds/ga.mp3'
+  },
+  {
+    hiragana: 'ぎ',
+    katakana: 'ギ',
+    roomaji: 'gi',
+    sound: '/sounds/gi.mp3'
+  },
+  {
+    hiragana: 'ぐ',
+    katakana: 'グ',
+    roomaji: 'gu',
+    sound: '/sounds/gu.mp3'
+  },
+  {
+    hiragana: 'げ',
+    katakana: 'ゲ',
+    roomaji: 'ge',
+    sound: '/sounds/ge.mp3'
+  },
+  {
+    hiragana: 'ご',
+    katakana: 'ゴ',
+    roomaji: 'go',
+    sound: '/sounds/go.mp3'
+  },
+  {
+    hiragana: 'ざ',
+    katakana: 'ザ',
+    roomaji: 'za',
+    sound: '/sounds/za.mp3'
+  },
+  {
+    hiragana: 'じ',
+    katakana: 'ジ',
+    roomaji: 'ji',
+    sound: '/sounds/ji.mp3'
+  },
+  {
+    hiragana: 'ず',
+    katakana: 'ズ',
+    roomaji: 'zu',
+    sound: '/sounds/zu.mp3'
+  },
+  {
+    hiragana: 'ぜ',
+    katakana: 'ゼ',
+    roomaji: 'ze',
+    sound: '/sounds/ze.mp3'
+  },
+  {
+    hiragana: 'ぞ',
+    katakana: 'ゾ',
+    roomaji: 'zo',
+    sound: '/sounds/zo.mp3'
+  },
+  {
+    hiragana: 'だ',
+    katakana: 'ダ',
+    roomaji: 'da',
+    sound: '/sounds/da.mp3'
+  },
+  {
+    hiragana: 'ぢ',
+    katakana: 'ヂ',
+    roomaji: 'ji',
+    sound: '/sounds/di.mp3'
+  },
+  {
+    hiragana: 'づ',
+    katakana: 'ヅ',
+    roomaji: 'zu',
+    sound: '/sounds/du.mp3'
+  },
+  {
+    hiragana: 'で',
+    katakana: 'デ',
+    roomaji: 'de',
+    sound: '/sounds/de.mp3'
+  },
+  {
+    hiragana: 'ど',
+    katakana: 'ド',
+    roomaji: 'do',
+    sound: '/sounds/do.mp3'
+  },
+  {
+    hiragana: 'ば',
+    katakana: 'バ',
+    roomaji: 'ba',
+    sound: '/sounds/ba.mp3'
+  },
+  {
+    hiragana: 'び',
+    katakana: 'ビ',
+    roomaji: 'bi',
+    sound: '/sounds/bi.mp3'
+  },
+  {
+    hiragana: 'ぶ',
+    katakana: 'ブ',
+    roomaji: 'bu',
+    sound: '/sounds/bu.mp3'
+  },
+  {
+    hiragana: 'べ',
+    katakana: 'ベ',
+    roomaji: 'be',
+    sound: '/sounds/be.mp3'
+  },
+  {
+    hiragana: 'ぼ',
+    katakana: 'ボ',
+    roomaji: 'bo',
+    sound: '/sounds/bo.mp3'
+  },
+  {
+    hiragana: 'ぱ',
+    katakana: 'パ',
+    roomaji: 'pa',
+    sound: '/sounds/pa.mp3'
+  },
+  {
+    hiragana: 'ぴ',
+    katakana: 'ピ',
+    roomaji: 'pi',
+    sound: '/sounds/pi.mp3'
+  },
+  {
+    hiragana: 'ぷ',
+    katakana: 'プ',
+    roomaji: 'pu',
+    sound: '/sounds/pu.mp3'
+  },
+  {
+    hiragana: 'ぺ',
+    katakana: 'ペ',
+    roomaji: 'pe',
+    sound: '/sounds/pe.mp3'
+  },
+  {
+    hiragana: 'ぽ',
+    katakana: 'ポ',
+    roomaji: 'po',
+    sound: '/sounds/po.mp3'
+  }
+];
+
+//拗音
+const youonn = [
+  {
+    hiragana: 'きゃ',
+    katakana: 'キャ',
+    roomaji: 'kya',
+    sound: '/sounds/kya.mp3'
+  },
+  {
+    hiragana: 'きゅ',
+    katakana: 'キュ',
+    roomaji: 'kyu',
+    sound: '/sounds/kyu.mp3'
+  },
+  {
+    hiragana: 'きょ',
+    katakana: 'キョ',
+    roomaji: 'kyo',
+    sound: '/sounds/kyo.mp3'
+  },
+  {
+    hiragana: 'しゃ',
+    katakana: 'シャ',
+    roomaji: 'sha',
+    sound: '/sounds/sha.mp3'
+  },
+  {
+    hiragana: 'しゅ',
+    katakana: 'シュ',
+    roomaji: 'shu',
+    sound: '/sounds/shu.mp3'
+  },
+  {
+    hiragana: 'しょ',
+    katakana: 'ショ',
+    roomaji: 'sho',
+    sound: '/sounds/sho.mp3'
+  },
+  {
+    hiragana: 'ちゃ',
+    katakana: 'チャ',
+    roomaji: 'cha',
+    sound: '/sounds/cha.mp3'
+  },
+  {
+    hiragana: 'ちゅ',
+    katakana: 'チュ',
+    roomaji: 'chu',
+    sound: '/sounds/chu.mp3'
+  },
+  {
+    hiragana: 'ちょ',
+    katakana: 'チョ',
+    roomaji: 'cho',
+    sound: '/sounds/cho.mp3'
+  },
+  {
+    hiragana: 'にゃ',
+    katakana: 'ニャ',
+    roomaji: 'nya',
+    sound: '/sounds/nya.mp3'
+  },
+  {
+    hiragana: 'にゅ',
+    katakana: 'ニュ',
+    roomaji: 'nyu',
+    sound: '/sounds/nyu.mp3'
+  },
+  {
+    hiragana: 'にょ',
+    katakana: 'ニョ',
+    roomaji: 'nyo',
+    sound: '/sounds/nyo.mp3'
+  },
+  {
+    hiragana: 'ひゃ',
+    katakana: 'ヒャ',
+    roomaji: 'hya',
+    sound: '/sounds/hya.mp3'
+  },
+  {
+    hiragana: 'ひゅ',
+    katakana: 'ヒュ',
+    roomaji: 'hyu',
+    sound: '/sounds/hyu.mp3'
+  },
+  {
+    hiragana: 'ひょ',
+    katakana: 'ヒョ',
+    roomaji: 'hyo',
+    sound: '/sounds/hyo.mp3'
+  },
+  {
+    hiragana: 'みゃ',
+    katakana: 'ミャ',
+    roomaji: 'mya',
+    sound: '/sounds/mya.mp3'
+  },
+  {
+    hiragana: 'みゅ',
+    katakana: 'ミュ',
+    roomaji: 'myu',
+    sound: '/sounds/myu.mp3'
+  },
+  {
+    hiragana: 'みょ',
+    katakana: 'ミョ',
+    roomaji: 'myo',
+    sound: '/sounds/myo.mp3'
+  },
+  {
+    hiragana: 'りゃ',
+    katakana: 'リャ',
+    roomaji: 'rya',
+    sound: '/sounds/rya.mp3'
+  },
+  {
+    hiragana: 'りゅ',
+    katakana: 'リュ',
+    roomaji: 'ryu',
+    sound: '/sounds/ryu.mp3'
+  },
+  {
+    hiragana: 'りょ',
+    katakana: 'リョ',
+    roomaji: 'ryo',
+    sound: '/sounds/ryo.mp3'
+  },
+  {
+    hiragana: 'ぎゃ',
+    katakana: 'ギャ',
+    roomaji: 'gya',
+    sound: '/sounds/gya.mp3'
+  },
+  {
+    hiragana: 'ぎゅ',
+    katakana: 'ギュ',
+    roomaji: 'gyu',
+    sound: '/sounds/gyu.mp3'
+  },
+  {
+    hiragana: 'ぎょ',
+    katakana: 'ギョ',
+    roomaji: 'gyo',
+    sound: '/sounds/gyo.mp3'
+  },
+  {
+    hiragana: 'じゃ',
+    katakana: 'ジャ',
+    roomaji: 'ja',
+    sound: '/sounds/ja.mp3'
+  },
+  {
+    hiragana: 'じゅ',
+    katakana: 'ジュ',
+    roomaji: 'ju',
+    sound: '/sounds/ju.mp3'
+  },
+  {
+    hiragana: 'じょ',
+    katakana: 'ジョ',
+    roomaji: 'jo',
+    sound: '/sounds/jo.mp3'
+  },
+  {
+    hiragana: 'びゃ',
+    katakana: 'ビャ',
+    roomaji: 'bya',
+    sound: '/sounds/bya.mp3'
+  },
+  {
+    hiragana: 'びゅ',
+    katakana: 'ビュ',
+    roomaji: 'byu',
+    sound: '/sounds/byu.mp3'
+  },
+  {
+    hiragana: 'びょ',
+    katakana: 'ビョ',
+    roomaji: 'byo',
+    sound: '/sounds/byo.mp3'
+  },
+  {
+    hiragana: 'ぴゃ',
+    katakana: 'ピャ',
+    roomaji: 'pya',
+    sound: '/sounds/pya.mp3'
+  },
+  {
+    hiragana: 'ぴゅ',
+    katakana: 'ピュ',
+    roomaji: 'pyu',
+    sound: '/sounds/pyu.mp3'
+  },
+  {
+    hiragana: 'ぴょ',
+    katakana: 'ピョ',
+    roomaji: 'pyo',
+    sound: '/sounds/pyo.mp3'
+  }
+];
+
+const gojuon = {
+  seionn,
+  dakuonn,
+  youonn
+};
+
+export type GojuonType = typeof gojuon;
+export type SeionnType = typeof seionn;
+export type DakuonnType = typeof dakuonn;
+export type YouonnType = typeof youonn;
+
+export default gojuon;
